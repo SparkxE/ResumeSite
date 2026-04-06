10ae2e4c6a73e08245db3f9d15e47b64b5ab0a8e
Aaron Anderson <sylverblade16@gmail.com>
2026-04-06 18:35:24 -0500

    Started working on adding mobile-specific styling to pages; adjusted various elements in MobileStyle.css and relevant JSX pages to better fit Mobile form-factor.

---
+++ src/isMobile.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export function isMobile(mobileMaxSize = 801) {
+    const [isMobile, setMobile] = useState(undefined);
+
+    useEffect(() => {
+        
+        //media query that matches screens smaller than mobileMaxSize
+        const windowSizeQuery = window.matchMedia(`(max-width: ${mobileMaxSize - 1}px)`);
+
+        //listener for changes in screen size
+        const onChange = () => {
+            setMobile(window.innerWidth < mobileMaxSize);
+        };
+
+        //utilize listener to update as-needed
+        windowSizeQuery.addEventListener("change", onChange);
+
+        setMobile(window.innerWidth < mobileMaxSize);
+
+        //remove listener when unloaded
+        return () => {
+            windowSizeQuery.removeEventListener("change", onChange);
+        };
+    }, [mobileMaxSize]);
+
+    //return boolean using !! to catch undefined values as false
+    return !!isMobile;
+}
