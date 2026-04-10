3fa527770f89edd5503d6273de8cda86b2cb7590
Aaron Anderson <sylverblade16@gmail.com>
2026-04-10 18:31:09 -0500

    Adjusted max window size value for displaying mobile styling

--- src/isMobile.jsx
+++ src/isMobile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export function isMobile(mobileMaxSize = 801) {
+export function isMobile(mobileMaxSize = 1001) {
     const [isMobile, setMobile] = useState(undefined);
 
     useEffect(() => {
